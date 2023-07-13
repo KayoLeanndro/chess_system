3e522a2b44a2ab16a89c74b7ba54a0717d6fc0ef
Kayo Leanndro <kayoleanndro2@gmail.com>
2023-07-13 13:30:37 -0300

    defensive programming

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,10 @@
 package application;
-
-import boardgame.Board;
 import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+	
 		ChessMatch chessMatch = new ChessMatch();
 		Ui.printBoard(chessMatch.getPieces());
 		
---
+++ src/boardgame/BoardExcepcion.java
@@ -0,0 +1,9 @@
+package boardgame;
+
+public class BoardExcepcion extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public BoardExcepcion(String msg){
+        super(msg);
+    }
+}
