e6ac93583486be272c7a1b0117d3aa06cda4d7ed
Kayo Leanndro <kayoleanndro2@gmail.com>
2023-07-18 12:56:48 -0300

    implementing possible moves of rock

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -1,9 +1,10 @@
 package chess;
 import boardgame.Piece;
+import boardgame.Position;
 import boardgame.Board;
 
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
     
     private Color color;
     
@@ -14,6 +15,11 @@ public class ChessPiece extends Piece {
 
     public Color getColor() {
         return color;
+    }
+
+    protected boolean isThereOpponentPiece(Position position){
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p != null && p.getColor() != color;
     }
 
 
